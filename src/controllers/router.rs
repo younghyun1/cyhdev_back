22619d3e714a9eb87330c8691e6fd56f0d76d2d4
Younghyun Ji <younghyun1@gmail.com>
2024-12-10 18:32:43 +0900

    [feature] auth

--- src/controllers/router.rs
+++ src/controllers/router.rs
@@ -5,11 +5,15 @@ use tower_http::compression::CompressionLayer;
 
 use crate::utils::server_init::server_state_def::ServerState;
 
-use super::{auth::signup::signup, middleware::request_response_info::print_request_info};
+use super::{
+    auth::{signup::signup, verify_email::verify_email},
+    middleware::request_response_info::print_request_info,
+};
 
 pub fn generate_router(state: &Arc<ServerState>) -> axum::Router {
     axum::Router::new()
-        .route("/auth/signup", post(signup))
+        .route("/api/auth/signup", post(signup))
+        .route("/api/auth/verify_email", post(verify_email))
         .layer(CompressionLayer::new())
         .layer(from_fn(print_request_info))
         .with_state(Arc::clone(state))
